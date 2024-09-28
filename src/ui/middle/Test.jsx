2d4526bc86acefefda102a4e8bdf5c9ea275a554
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2024-09-28 17:59:01 +0200

    buttons material design colors

--- src/ui/middle/Test.jsx
+++ src/ui/middle/Test.jsx
@@ -1,7 +1,25 @@
 export default function Application() {
     return (
-        <p>
-            Test
-        </p>
+        <div class="tabs three">
+            <input id='tab-1' type='radio' name='tabgroupB' checked />
+            <label class="pseudo button toggle" for="tab-1">Forest</label>
+            <input id='tab-2' type='radio' name='tabgroupB' />
+            <label class="pseudo button toggle" for="tab-2">Lake</label>
+            <input id='tab-3' type='radio' name='tabgroupB' />
+            <label class="pseudo button toggle" for="tab-3">Balloon</label>
+            <div class="row">
+                <div>
+                    Forest
+                </div>
+
+                <div>
+                    Lake
+                </div>
+
+                <div>
+                    Baloo
+                </div>
+            </div>
+        </div>
     )
 }
